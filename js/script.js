3875dd605f82955fa37df41f8100be066fd4012b
userSaGithuba <emailSaGithuba>
2022-11-11 22:39:04 +0100

    kuldugmici

--- js/script.js
+++ js/script.js
@@ -1,12 +1,25 @@
+function deleteSomething() {
+  alert("Brisanje unosa...");
+  document.getElementById("imeprezime").value = "";
+  document.getElementById("telefon").value = "";
+  document.getElementById("adresa").value = "";
+  document.getElementById("napomena").value = "";
+ 
+  document.querySelectorAll('input[type="checkbox"]')
+    .forEach(el => el.checked = false);
+}
 
-
-
-
-function validateForm() {
-    var x = document.forms[imeprezime].value;
-    if (x == "") {
-      alert("Name must be filled out");
-      return false;
-    }
+function Klik() {
+if(
+  document.getElementById("imeprezime").value != "" &
+  document.getElementById("telefon").value != "" &
+  document.getElementById("adresa").value != ""){
+    if(confirm("Vaši ime:\n" + (document.getElementById('imeprezime').innerHTML = document.getElementById("imeprezime").value) + "\nVaš broj telefona:\n " + (document.getElementById('telefon').innerHTML = 
+                    document.getElementById("telefon").value) + "\nVaša adresa:\n " + (document.getElementById('adresa').innerHTML = 
+                    document.getElementById("adresa").value))){
+    alert("Vaša porudžbina je zabeležena!");} 
+    else deleteSomething();
   }
+  else alert("Niste popunili sva polja!");
+}
 
