01496743fa445c1313cf6b9a7c4d81b911eece8a
userSaGithuba <emailSaGithuba>
2022-11-12 22:01:36 +0100

    lakunoc

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ if(
   document.getElementById("imeprezime").value != "" &
   document.getElementById("telefon").value != "" &
   document.getElementById("adresa").value != ""){
-    if(confirm("Vaši ime:\n" + (document.getElementById('imeprezime').innerHTML = document.getElementById("imeprezime").value) + "\nVaš broj telefona:\n " + (document.getElementById('telefon').innerHTML = 
+    if(confirm("Vaše ime:\n" + (document.getElementById('imeprezime').innerHTML = document.getElementById("imeprezime").value) + "\nVaš broj telefona:\n " + (document.getElementById('telefon').innerHTML = 
                     document.getElementById("telefon").value) + "\nVaša adresa:\n " + (document.getElementById('adresa').innerHTML = 
                     document.getElementById("adresa").value))){
     alert("Vaša porudžbina je zabeležena!");
