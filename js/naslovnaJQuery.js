34784a12ee97e42a6f883debd2be918fe5d71237
pozitiva <ivadjokovic2@gmail.com>
2022-11-20 03:06:23 +0100

    propala galerija

--- js/naslovnaJQuery.js
+++ js/naslovnaJQuery.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $(".iva").mouseenter(function(){
+    $(".dugme_poruci").mouseenter(function(){
       alert("Kliknite ovo dugme da biste poručili neki od naših proizvoda na kućnu adresu!");
     });
   });
